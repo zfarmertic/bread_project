68317da874480d865d00f956bb3def8153ead8e8
Johnnykeysofficial <johnnykeysofficiall@gmail.com>
2024-08-17 13:37:28 +0100

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../components/Home.css"
+
+const Home = () => {
+  return (
+    <div>
+
+      <header>
+
+    
+
+
+      </header>
+      
+    </div>
+  )
+}
+
+export default Home
